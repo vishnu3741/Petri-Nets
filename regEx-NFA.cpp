bbd5ab9aa800e36a800ca6e984f2409a162fbedb
vishnu374 <vtpalavalasa@gmail.com>
2019-11-03 15:18:32 +0530

    fixed regEx-NFA conversion

--- regEx-NFA.cpp
+++ regEx-NFA.cpp
@@ -1,227 +1,241 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<set>
-#include<stack>
-
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <list>
+#include <map>
+#include <vector>
 using namespace std;
 
+int operatorPrecedence(char c){
+  if(c=='+') return 0;
+  else if(c=='.') return 1;
+  else if(c=='*') return 2;
+}
 
-struct trans {
-	int vertex_from;
-	int vertex_to;
-	char trans_symbol;
-};
+string toPostfix(string regEx){
+  string postFix;
+  stack<char> operatorStack;
 
+  for(int i=0;i<regEx.size();i++){
+    if(regEx[i]=='.' || regEx[i]=='+' || regEx[i]=='*'){
+      while(!operatorStack.empty() && operatorStack.top()!='(' && (operatorPrecedence(operatorStack.top()) >= operatorPrecedence(regEx[i]))){
+	postFix=postFix+operatorStack.top();
+	operatorStack.pop();
+      }
+      operatorStack.push(regEx[i]);
+    }
+    else if(regEx[i]=='(' || regEx[i]==')'){
+      if(regEx[i]=='(') operatorStack.push(regEx[i]);
+      else{
+	while(operatorStack.top() != '('){
+	  postFix=postFix+operatorStack.top();
+	  operatorStack.pop();
+	}
+	operatorStack.pop();
+      }
+    }
+    else postFix=postFix+regEx[i];
+  }
 
-class NFA {
+  while(!operatorStack.empty()){
+    postFix=postFix+operatorStack.top();
+    operatorStack.pop();
+  }
+
+  return postFix;
+  
+}
+
+class state{
 public:
-	vector<int> vertex;
-	vector<trans> transitions;
-	int final_state;
+  int ID;
+  bool ifFinal;
+  map<char,vector<state*> > transitions;
 
-	NFA() {
-
-	}
-
-	int get_vertex_count() {
-		return vertex.size();
-	}
-
-	void set_vertex(int no_vertex) {
-		for(int i = 0; i < no_vertex; i++) {
-			vertex.push_back(i);
-		}
-	}
-
-	void set_transition(int vertex_from, int vertex_to, char trans_symbol) {
-		trans new_trans;
-		new_trans.vertex_from = vertex_from;
-		new_trans.vertex_to = vertex_to;
-		new_trans.trans_symbol = trans_symbol;
-		transitions.push_back(new_trans);
-	}
-
-	void set_final_state(int fs) {
-		final_state = fs;
-	}
-
-	int get_final_state() {
-		return final_state;
-	}
-
-	void display() {
-		trans new_trans;
-		cout<<"\n";
-		for(int i = 0; i < transitions.size(); i++) {
-			new_trans = transitions.at(i);
-			cout<<new_trans.vertex_from<<" "<<new_trans.trans_symbol<<" "<<new_trans.vertex_to<<endl;
-		}
-		cout<<"\nThe final state is "<<get_final_state()<<endl;
-	}
+  void setValues(int ID,bool ifFinal){
+    this->ID=ID;
+    this->ifFinal=ifFinal;
+    return;
+  }
+  int getID(){
+    return this->ID;
+  }
+  void setID(int id){
+    this->ID=id;
+    return;
+  }
+  void setIfFinal(bool ifFinal){
+    this->ifFinal=ifFinal;
+    return;
+  }
+  void addTransition(char letter,state* s){
+    this->transitions[letter].push_back(s);
+    return;
+  }
+  void addEpsilonTransition(state* s){
+    this->transitions['#'].push_back(s);
+    return;
+  }
 };
 
-
-
-NFA concat(NFA a, NFA b) {
-	NFA result;
-	result.set_vertex(a.get_vertex_count() + b.get_vertex_count());
-	int i;
-	trans new_trans;
-
-	for(i = 0; i < a.transitions.size(); i++) {
-		new_trans = a.transitions.at(i);
-		result.set_transition(new_trans.vertex_from, new_trans.vertex_to, new_trans.trans_symbol);
-	}
-
-	result.set_transition(a.get_final_state(), a.get_vertex_count(), '#');
-
-	for(i = 0; i < b.transitions.size(); i++) {
-		new_trans = b.transitions.at(i);
-		result.set_transition(new_trans.vertex_from + a.get_vertex_count(), new_trans.vertex_to + a.get_vertex_count(), new_trans.trans_symbol);
-	}
-
-	result.set_final_state(a.get_vertex_count() + b.get_vertex_count() - 1);
-
-	return result;
+bool checkElement(vector<state*> visited,state* s){
+  for(int i=0;i<visited.size();i++) if(visited[i]==s) return true;
+  return false;
 }
 
+class NFA{
+public:
+  state* startState;
+  state* endState;
 
-NFA kleene(NFA a) {
-	NFA result;
-	int i;
-	trans new_trans;
-	
-	result.set_vertex(a.get_vertex_count() + 2);
-
-	result.set_transition(0, 1, '#');
-
-	for(i = 0; i < a.transitions.size(); i++) {
-		new_trans = a.transitions.at(i);
-		result.set_transition(new_trans.vertex_from + 1, new_trans.vertex_to + 1, new_trans.trans_symbol);
+  void setStartState(state* s){
+    this->startState=s;
+    return;
+  }
+  void setEndState(state* s){
+    this->endState=s;
+    return;
+  }
+  void printNFA(){
+    cout<<"Printing NFA : "<<endl;
+    cout<<"Start State : "<<startState->ID<<endl;
+    cout<<"End State : "<<endState->ID<<endl;
+    cout<<'\n';
+    queue<state*> list;
+    vector<state*> visited;
+    list.push(this->startState);
+    
+    while(!list.empty()){
+      state* n=list.front();
+      visited.push_back(n);
+      list.pop();
+      if(n->transitions.size()>0)cout<<"State : "<<n->ID<<endl;
+      map<char,vector<state*> >::iterator itr;
+      for(itr=n->transitions.begin(); itr!=n->transitions.end();itr++){
+	for(int i=0;i<itr->second.size();i++){
+	  cout<<n->ID<<"-"<<itr->first<<"->"<<itr->second[i]->ID<<endl;
+	  if(!checkElement(visited,itr->second[i])) list.push(itr->second[i]);
 	}
+      }
+    }
+    cout<<"print done"<<endl;
+  }
+};
 
-	result.set_transition(a.get_vertex_count(), a.get_vertex_count() + 1, '#');
-	result.set_transition(a.get_vertex_count(), 1, '#');
-	result.set_transition(0, a.get_vertex_count() + 1, '#');
-
-	result.set_final_state(a.get_vertex_count() + 1);
-
-	return result;
+NFA fromEpsilon(int &ID){
+  state *start=new state;
+  state *final=new state;
+  start->setValues(ID,false);
+  final->setValues(ID+1,true);
+  ID+=2;
+  NFA *retNFA=new NFA;
+  start->addEpsilonTransition(final);
+  retNFA->setStartState(start);
+  retNFA->setEndState(final);
+  return *retNFA;
 }
 
-
-NFA or_selection(vector<NFA> selections, int no_of_selections) {
-	NFA result;
-	int vertex_count = 2;
-	int i, j;
-	NFA med;
-	trans new_trans;
-
-	for(i = 0; i < no_of_selections; i++) {
-		vertex_count += selections.at(i).get_vertex_count();
-	}
-
-	result.set_vertex(vertex_count);
-	
-	int adder_track = 1;
-
-	for(i = 0; i < no_of_selections; i++) {
-		result.set_transition(0, adder_track, '#');
-		med = selections.at(i);
-		for(j = 0; j < med.transitions.size(); j++) {
-			new_trans = med.transitions.at(j);
-			result.set_transition(new_trans.vertex_from + adder_track, new_trans.vertex_to + adder_track, new_trans.trans_symbol);
-		}
-		adder_track += med.get_vertex_count();
-
-		result.set_transition(adder_track - 1, vertex_count - 1, '#');
-	}
-
-	result.set_final_state(vertex_count - 1);
-
-	return result;
+NFA fromSymbol(char letter,int &ID){
+  state *start=new state;
+  state *final=new state;
+  start->setValues(ID,false);
+  final->setValues(ID+1,true);
+  ID+=2;
+  NFA *retNFA=new NFA;
+  start->addTransition(letter,final);
+  retNFA->setStartState(start);
+  retNFA->setEndState(final);
+  return *retNFA;
 }
 
-
-NFA re_to_nfa(string re) {
-	stack<char> operators;
-	stack<NFA> operands;
-	char op_sym;
-	int op_count;
-	char cur_sym;
-	NFA *new_sym;
-	
-	for(string::iterator it = re.begin(); it != re.end(); ++it) {
-		cur_sym = *it;
-		if(cur_sym != '(' && cur_sym != ')' && cur_sym != '*' && cur_sym != '|' && cur_sym != '.') {
-			new_sym = new NFA();
-			new_sym->set_vertex(2);
-			new_sym->set_transition(0, 1, cur_sym);
-			new_sym->set_final_state(1);
-			operands.push(*new_sym);
-			delete new_sym;
-		} else {
-			if(cur_sym == '*') {
-				NFA star_sym = operands.top();
-				operands.pop();
-				operands.push(kleene(star_sym));
-			} else if(cur_sym == '.') {
-				operators.push(cur_sym);
-			} else if(cur_sym == '|') {
-				operators.push(cur_sym);
-			} else if(cur_sym == '(') {
-				operators.push(cur_sym);
-			} else {
-				op_count = 0;
-				char c;
-				op_sym = operators.top();
-				if(op_sym == '(') continue;
-				do {
-					operators.pop();
-					op_count++;
-				} while(operators.top() != '(');
-				operators.pop();
-				NFA op1;
-				NFA op2;
-				vector<NFA> selections;
-				if(op_sym == '.') {
-					for(int i = 0; i < op_count; i++) {
-						op2 = operands.top();
-						operands.pop();
-						op1 = operands.top();
-						operands.pop();
-						operands.push(concat(op1, op2));
-					}
-				} else if(op_sym == '|'){
-					selections.assign(op_count + 1, NFA());
-					int tracker = op_count;
-					for(int i = 0; i < op_count + 1; i++) {
-						selections.at(tracker) = operands.top();
-						tracker--;
-						operands.pop();
-					}
-					operands.push(or_selection(selections, op_count+1));
-				} else {
-					
-				}
-			}
-		}
-	}
-
-	return operands.top();
+NFA concatOP(NFA first,NFA second){
+  first.endState->addEpsilonTransition(second.startState);
+  first.endState->setIfFinal(false);
+  NFA *retNFA=new NFA;
+  retNFA->setStartState(first.startState);
+  retNFA->setEndState(second.endState);
+  return *(retNFA);
 }
 
+NFA unionOP(NFA first,NFA second,int &ID){
+  state *start=new state;
+  start->setValues(ID,false);
+  start->addEpsilonTransition(first.startState);
+  start->addEpsilonTransition(second.startState);
 
-int main() {	
+  state* end=new state;
+  end->setValues(ID+1,true);
+  ID+=2;
+  first.endState->addEpsilonTransition(end);
+  first.endState->ifFinal=false;
+  second.endState->addEpsilonTransition(end);
+  second.endState->ifFinal=false;
 
-	string re;
-	set<char> symbols;
-	cin>>re;
-	
-	NFA required_nfa;
-	required_nfa = re_to_nfa(re);
-	required_nfa.display();	
-
-	return 0;
+  NFA* retNFA=new NFA;
+  retNFA->setStartState(start);
+  retNFA->setEndState(end);
+  return *(retNFA);
 }
+
+NFA closureOP(NFA &curNFA,int &ID){
+  state* start=new state;
+  start->setValues(ID,false);
+  state* end=new state;
+  end->setValues(ID+1,true);
+  ID+=2;
+
+  curNFA.endState->addEpsilonTransition(curNFA.startState);
+  curNFA.endState->ifFinal=false;
+  curNFA.endState->addEpsilonTransition(end);
+  start->addEpsilonTransition(end);
+  start->addEpsilonTransition(curNFA.startState);
+  
+  NFA* retNFA=new NFA;
+  retNFA->setStartState(start);
+  retNFA->setEndState(end);
+  return *(retNFA);
+}
+
+NFA toNFA(string postfixExp){
+  int ID=0;
+  if(postfixExp=="") return fromEpsilon(ID);
+  stack<NFA> nfaStack;
+  for(int i=0;i<postfixExp.size();i++){
+    if(postfixExp[i]=='*'){
+      NFA curNFA=nfaStack.top();
+      nfaStack.pop();
+      nfaStack.push(closureOP(curNFA,ID));
+    }
+    else if(postfixExp[i]=='+'){
+      NFA right=nfaStack.top();
+      nfaStack.pop();
+      NFA left=nfaStack.top();
+      nfaStack.pop();
+      nfaStack.push(unionOP(left,right,ID));
+    }
+    else if(postfixExp[i]=='.'){
+      NFA right=nfaStack.top();
+      nfaStack.pop();
+      NFA left=nfaStack.top();
+      nfaStack.pop();
+      nfaStack.push(concatOP(left,right));
+    }
+    else{
+      nfaStack.push(fromSymbol(postfixExp[i],ID));
+    }
+  }
+
+  return nfaStack.top();
+}
+
+int main(){
+  string regEX;
+  cin>>regEX;
+  string postfixExp=toPostfix(regEX);
+  cout<<'\n';
+  cout<<"The Postfix Expression : "<<postfixExp<<endl;
+  cout<<'\n';
+  NFA retNFA=toNFA(postfixExp);
+  retNFA.printNFA();
+}
+
